f6b7c7644f65eaa80054554d6386e7021f201313
Jeff Maughan <geophray@gmail.com>
2020-02-11 00:08:08 -0700

    Add China UnionPay & Switch cards to detectNetwork

--- detectNetwork.js
+++ detectNetwork.js
@@ -16,7 +16,7 @@ var detectNetwork = function(cardNumber) {
 
   // Create variables cardLength, and cardNetwork
   var cardLength = cardNumber.length;
-  var cardNetwork = "Unable to identify card network.";
+  var cardNetwork = 'Unable to identify card network.';
   // Check cardLength against cases. If no match, return default error message.
   switch (cardLength) {
   	case 12:
@@ -56,8 +56,10 @@ var detectNetwork = function(cardNumber) {
 	  	break;
 
   	case 16:
-  		// If cardLength is 16 check if prefix matches Visa, MasterCard, Discover, or Maestro
-  		if(isVisa(cardNumber)) {
+  		// If cardLength is 16 check if prefix matches Visa, MasterCard, Discover, Maestro, China UnionPay, or Switch
+  		if(isSwitch(cardNumber)) {
+  			cardNetwork = "Switch";
+  		} else if(isVisa(cardNumber)) {
   			cardNetwork = "Visa";
   		} else if(isMasterCard(cardNumber)) {
   			cardNetwork = "MasterCard";
@@ -65,31 +67,43 @@ var detectNetwork = function(cardNumber) {
   			cardNetwork = "Discover";
   		} else if(isMaestro(cardNumber)) {
   			cardNetwork = "Maestro";
+  		} else if(isChinaUnionPay(cardNumber)) {
+  			cardNetwork = "China UnionPay";
   		}
   		break;
 
   	case 17:
-  		// If cardLength is 19 check if prefix matches Maestro
+  		// If cardLength is 19 check if prefix matches Maestro, or China UnionPay
   		if(isMaestro(cardNumber)) {
   			cardNetwork = "Maestro";
+  		} else if(isChinaUnionPay(cardNumber)) {
+  			cardNetwork = "China UnionPay";
   		}
   		break;
 
   	case 18:
-  		// If cardLength is 19 check if prefix matches Maestro
-  		if(isMaestro(cardNumber)) {
+  		// If cardLength is 19 check if prefix matches Maestro, China UnionPay, or Switch
+  		if(isSwitch(cardNumber)) {
+  			cardNetwork = "Switch";
+  		} else if(isMaestro(cardNumber)) {
   			cardNetwork = "Maestro";
+  		} else if(isChinaUnionPay(cardNumber)) {
+  			cardNetwork = "China UnionPay";
   		}
   		break;
 
   	case 19:
-  		// If cardLength is 19 check if prefix matches Visa, Discover, or Maestro
-  		if(isVisa(cardNumber)) {
+  		// If cardLength is 19 check if prefix matches Visa, Discover, Maestro, China UnionPay, or Switch
+  		if(isSwitch(cardNumber)) {
+  			cardNetwork = "Switch";
+  		} else if(isVisa(cardNumber)) {
   			cardNetwork = "Visa";
   		} else if(isDiscover(cardNumber)) {
   			cardNetwork = "Discover";
   		} else if(isMaestro(cardNumber)) {
   			cardNetwork = "Maestro";
+  		} else if(isChinaUnionPay(cardNumber)) {
+  			cardNetwork = "China UnionPay";
   		}
   		break;
 
@@ -103,6 +117,7 @@ var detectNetwork = function(cardNumber) {
 //Create helper functions to check prefixes
 var isDinersClub = function(cardNumber) {
   	var cardPrefix = cardNumber.slice(0, 2);
+
 	if(cardPrefix === '38' || cardPrefix === '39') {
 		return true;
 	} 
@@ -110,6 +125,7 @@ var isDinersClub = function(cardNumber) {
 
 var isAmericanExpress = function(cardNumber) {
 	var cardPrefix = cardNumber.slice(0, 2);
+
 	if(cardPrefix === '34' || cardPrefix === '37') {
 		return true;
 	} 
@@ -117,6 +133,7 @@ var isAmericanExpress = function(cardNumber) {
 
 var isVisa = function(cardNumber) {
 	var cardPrefix = cardNumber.slice(0, 1);
+
 	if(cardPrefix === '4') {
 		return true;
 	}
@@ -124,6 +141,7 @@ var isVisa = function(cardNumber) {
 
 var isMasterCard = function(cardNumber) {
 	var cardPrefix = cardNumber.slice(0, 2);
+
 	if(cardPrefix >= 51 && cardPrefix <= 55) {
 		return true;
 	}
@@ -133,6 +151,7 @@ var isDiscover = function(cardNumber) {
 	var first2Digits = cardNumber.slice(0, 2);
 	var first3Digits = cardNumber.slice(0, 3);
 	var first4Digits = cardNumber.slice(0, 4);
+
 	if(first2Digits === '65' || first3Digits >= 644 && first3Digits <= 649 || first4Digits === '6011') {
 		return true;
 	}
@@ -140,7 +159,33 @@ var isDiscover = function(cardNumber) {
 
 var isMaestro = function(cardNumber) {
 	var cardPrefix = cardNumber.slice(0, 4);
+
 	if(cardPrefix === '5018' || cardPrefix === '5020' || cardPrefix === '5038' || cardPrefix === '6304') {
 		return true;
+	}
+}
+
+var isChinaUnionPay = function(cardNumber) {
+	var first3Digits = cardNumber.slice(0, 3);
+	var first4Digits = cardNumber.slice(0, 4);
+	var first6Digits = cardNumber.slice(0, 6);
+
+	if(first3Digits >= 624 && first3Digits <= 626 ||
+		first4Digits >= 6282 && first4Digits <= 6288 ||
+		first6Digits >= 622126 && first6Digits <= 622925) {
+		return true;
+	}
+}
+
+var isSwitch = function(cardNumber) {
+	// Create array of valid prefixes belonging to Switch cards.
+  	var cardPrefixes = ['4903', '4905', '4911', '4936', '564182', '633110', '6333', '6759'];
+	// Iterate over array of prefixes
+	for(let j = 0; j < cardPrefixes.length; j++) {
+		// If prefix is at the beginning of cardNumber
+		if(cardNumber.indexOf(cardPrefixes[j]) === 0) {
+			// return TRUE
+			return true;
+		}
 	}
 }
